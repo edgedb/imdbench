0ebc53bb370f79dbdb037677359a85d7bbd56819
Victor Petrovykh <victor@magic.io>
2019-02-26 21:26:47 -0500

    Add a script to initialize edgedb_bench database.

---
+++ flask_edgedb/initdb.py
@@ -0,0 +1,31 @@
+import edgedb
+
+
+if __name__ == '__main__':
+    con = edgedb.connect(user='edgedb', database='edgedb')
+
+    # check if the DB exists
+    res = con.fetch("""
+        SELECT schema::Database.name
+        FILTER schema::Database.name = 'edgedb_bench';
+    """)
+
+    if res:
+        con.execute("""
+            DROP DATABASE edgedb_bench;
+        """)
+
+    con.execute("""
+        CREATE DATABASE edgedb_bench;
+    """)
+
+    with open('flask_edgedb/default.eschema') as f:
+        schema = f.read()
+
+    con = edgedb.connect(user='edgedb', database='edgedb_bench')
+    con.execute(f'''
+        START TRANSACTION;
+        CREATE MIGRATION default::d0 TO eschema $${schema}$$;
+        COMMIT MIGRATION default::d0;
+        COMMIT;
+    ''')
