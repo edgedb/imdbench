2be48f7de87d5f5c54d14c7272578bb89c6a3222
Victor Petrovykh <victor@magic.io>
2019-06-25 18:11:32 -0400

    Clean up sequelize benchamrk a bit.
    Add more indexes to fix a pathology in "get_person" benchmark.
    Make it possible to set the logging parameter externally.

--- _sequelize/loaddata.js
+++ _sequelize/loaddata.js
@@ -1,28 +1,29 @@
 "use strict";
 
-const Sequelize = require('sequelize');
-const {App} = require("./models.js");
+const Sequelize = require("sequelize");
+const { App } = require("./models.js");
 
-const fs = require('fs');
-const _ = require('lodash');
-const ProgressBar = require('progress');
+const fs = require("fs");
+const _ = require("lodash");
+const ProgressBar = require("progress");
 
 const BATCH_SIZE = 1000;
-
 
 async function bulk_insert(app, data, modelName) {
   let dataName = modelName.toLowerCase();
   let adj = true;
   var bar = new ProgressBar(
-    _.padEnd(modelName, 31, ' ') + '|:bar| :current/:total\t:elapsed sec',
-    {total: data[dataName].length, width: 32}
+    _.padEnd(modelName, 31, " ") + "|:bar| :current/:total\t:elapsed sec",
+    { total: data[dataName].length, width: 32 }
   );
   bar.tick();
 
   for (var chunk of _.chunk(data[dataName], BATCH_SIZE)) {
-    await app.transaction(async function (Model, chunk, t) {
-      await Model.bulkCreate(chunk);
-    }.bind(null, app.models[modelName], chunk));
+    await app.transaction(
+      async function(Model, chunk, t) {
+        await Model.bulkCreate(chunk);
+      }.bind(null, app.models[modelName], chunk)
+    );
 
     if (adj) {
       bar.tick(chunk.length - 1);
@@ -33,25 +34,23 @@ async function bulk_insert(app, data, modelName) {
   }
 }
 
-
 async function main() {
   if (process.argv.length != 3) {
-    throw new Error(
-      'Correct usage: node _sequelize/loaddata.js dataset.json');
+    throw new Error("Correct usage: node _sequelize/loaddata.js dataset.json");
   }
 
   const dataset_path = process.argv[2];
-  var dataset = JSON.parse(fs.readFileSync(dataset_path, 'utf8'));
+  var dataset = JSON.parse(fs.readFileSync(dataset_path, "utf8"));
   var data = {};
 
   for (let rec of dataset) {
-    let rtype = rec.model.split('.')[1];
+    let rtype = rec.model.split(".")[1];
     let datum = rec.fields;
     if (rec.pk !== undefined) {
       datum.id = rec.pk;
     }
     // convert datetime
-    if (rtype == 'review') {
+    if (rtype == "review") {
       datum.creation_time = new Date(datum.creation_time);
     }
 
@@ -66,26 +65,42 @@ async function main() {
       max: 10
     }
   });
-  await sequelize.sync({force: true});
+  await sequelize.sync({ force: true });
 
-  await bulk_insert(sequelize, data, 'User');
-  await bulk_insert(sequelize, data, 'Person');
-  await bulk_insert(sequelize, data, 'Movie');
-  await bulk_insert(sequelize, data, 'Review');
-  await bulk_insert(sequelize, data, 'Directors');
-  await bulk_insert(sequelize, data, 'Cast');
+  await bulk_insert(sequelize, data, "User");
+  await bulk_insert(sequelize, data, "Person");
+  await bulk_insert(sequelize, data, "Movie");
+  await bulk_insert(sequelize, data, "Review");
+  await bulk_insert(sequelize, data, "Directors");
+  await bulk_insert(sequelize, data, "Cast");
 
   // creating these particular indexes is awkward in the model description
-  await sequelize.getQueryInterface().addIndex('Review', {
-    using: 'btree',
-    fields: ['author_id'],
+  await sequelize.getQueryInterface().addIndex("Review", {
+    using: "btree",
+    fields: ["author_id"]
   });
-  await sequelize.getQueryInterface().addIndex('Review', {
-    using: 'btree',
-    fields: ['movie_id'],
+  await sequelize.getQueryInterface().addIndex("Review", {
+    using: "btree",
+    fields: ["movie_id"]
+  });
+  await sequelize.getQueryInterface().addIndex("Cast", {
+    using: "btree",
+    fields: ["person_id"]
+  });
+  await sequelize.getQueryInterface().addIndex("Cast", {
+    using: "btree",
+    fields: ["movie_id"]
+  });
+  await sequelize.getQueryInterface().addIndex("Directors", {
+    using: "btree",
+    fields: ["person_id"]
+  });
+  await sequelize.getQueryInterface().addIndex("Directors", {
+    using: "btree",
+    fields: ["movie_id"]
   });
 
-  console.log('Models created.');
+  console.log("Models created.");
 }
 
 main();
