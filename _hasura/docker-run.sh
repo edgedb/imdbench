16dde82e497752a1ebef910236742b2d3c128994
Elvis Pranskevichus <elvis@edgedb.com>
2020-01-14 23:07:31 -0500

    Bump hasura

--- _hasura/docker-run.sh
+++ _hasura/docker-run.sh
@@ -14,4 +14,4 @@ docker run -d --net=host --name hasura-bench --rm \
        -e HASURA_GRAPHQL_DATABASE_URL=postgres://postgres_bench:edgedbbenchmark@localhost:5432/postgres_bench \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
        -e HASURA_GRAPHQL_PG_CONNECTIONS=10 \
-       hasura/graphql-engine:v1.0.0
+       hasura/graphql-engine:v1.1.0-beta.1
