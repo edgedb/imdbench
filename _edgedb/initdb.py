d73921deef870ba3be21f0912405eb6177dc3ff3
fmoor <fmoor@gmx.com>
2020-10-21 15:04:35 -0700

    update migration command syntax
    I had to make the changes in this PR
    to load data into edgedb server version 1-alpha6
    (1.0a6.dev5056+g4fb6cf00-2020090700~focal.nightly)

--- _edgedb/initdb.py
+++ _edgedb/initdb.py
@@ -66,7 +66,8 @@ if __name__ == '__main__':
     con = edgedb.connect(user='edgedb', database='edgedb_bench')
     con.execute(f'''
         START TRANSACTION;
-        CREATE MIGRATION d0 TO {{ {schema} }};
-        COMMIT MIGRATION d0;
+        START MIGRATION TO {{ {schema} }};
+        POPULATE MIGRATION;
+        COMMIT MIGRATION;
         COMMIT;
     ''')
