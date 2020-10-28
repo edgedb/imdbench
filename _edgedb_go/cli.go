cffec37fa7483d6251fe4dc6216ed76cc1eba482
fmoor <fmoor@gmx.com>
2020-10-28 12:17:09 -0700

    add go repack benchmark

---
+++ _edgedb_go/cli.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+
+	"gopkg.in/alecthomas/kingpin.v2"
+)
+
+type Args struct {
+	Duration      time.Duration
+	Timeout       time.Duration
+	Warmup        time.Duration
+	Query         string
+	Ids           []string
+	Host          string
+	Port          int
+	Path          string
+	IdsAreInts    string
+	NSamples      int
+	Concurrency   int
+	Protocol      string
+	Serialization string
+}
+
+func parseOrFatal(seconds int) time.Duration {
+	duration, err := time.ParseDuration(fmt.Sprintf("%vs", seconds))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return duration
+}
+
+func parseArgs() Args {
+	var (
+		app = kingpin.New(
+			"golang-edgedb-http-bench",
+			"EdgeDB HTTP benchmark runner.")
+
+		concurrency = app.Flag(
+			"concurrency", "number of concurrent connections").Default("10").Int()
+
+		duration = app.Flag(
+			"duration", "duration of test in seconds").Default("30").Int()
+
+		timeout = app.Flag(
+			"timeout", "server timeout in seconds").Default("2").Int()
+
+		warmup = app.Flag(
+			"warmup-time",
+			"duration of warmup period for each benchmark in seconds",
+		).Default("5").Int()
+
+		_ = app.Flag(
+			"output-format",
+			"result output format",
+		).Default("text").Enum("text", "json")
+
+		host = app.Flag(
+			"host", "EdgeDB server host").Default("127.0.0.1").String()
+
+		port = app.Flag(
+			"port", "EdgeDB server port").Default("8080").Int()
+
+		serialization = app.Flag(
+			"serialization",
+			"json or repack",
+		).Required().Enum("json", "repack")
+
+		path = app.Flag(
+			"path", "GraphQL API path").Default("").String()
+
+		nsamples = app.Flag(
+			"nsamples", "Number of result samples to return").Default("10").Int()
+
+		idsAreInts = app.Flag(
+			"ids-are-ints",
+			"Whether or not ids are integers",
+		).Default("False").Enum("True", "False")
+
+		protocol = app.Flag(
+			"protocol",
+			"application protocol to use: http or edgedb",
+		).Required().Enum("http", "edgedb")
+
+		queryfile = app.Arg(
+			"queryfile",
+			"file to read benchmark query information from",
+		).Required().String()
+	)
+
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+
+	args := Args{
+		Duration:      parseOrFatal(*duration),
+		Timeout:       parseOrFatal(*timeout),
+		Warmup:        parseOrFatal(*warmup),
+		Host:          *host,
+		Port:          *port,
+		Path:          *path,
+		IdsAreInts:    *idsAreInts,
+		NSamples:      *nsamples,
+		Concurrency:   *concurrency,
+		Protocol:      *protocol,
+		Serialization: *serialization,
+	}
+
+	file := os.Stdin
+	if *queryfile != "-" {
+		var err error
+		file, err = os.Open(*queryfile)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	bts, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	json.Unmarshal(bts, &args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return args
+}
---
+++ _edgedb_go/http.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/valyala/fasthttp"
+)
+
+func httpWorker(args Args) (Exec, Close) {
+	url := fmt.Sprintf("http://%s:%d%s", args.Host, args.Port, args.Path)
+
+	client := &fasthttp.Client{}
+	rsp := fasthttp.AcquireResponse()
+	req := fasthttp.AcquireRequest()
+	req.Header.SetMethod("POST")
+	req.Header.SetContentType("application/json")
+	req.SetRequestURI(url)
+
+	var payload struct {
+		Query     string                 `json:"query"`
+		Variables map[string]interface{} `json:"variables"`
+	}
+
+	payload.Query = args.Query
+	payload.Variables = make(map[string]interface{}, 0)
+
+	exec := func(id string) (time.Duration, string) {
+		start := time.Now()
+
+		if args.IdsAreInts == "True" {
+			var err error
+			payload.Variables["id"], err = strconv.Atoi(id)
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			payload.Variables["id"] = id
+		}
+
+		bts, err := json.Marshal(payload)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		req.SetBody(bts)
+		err = client.Do(req, rsp)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		sample := string(rsp.Body())
+		return time.Since(start), sample
+	}
+
+	close := func() {
+		fasthttp.ReleaseResponse(rsp)
+		fasthttp.ReleaseRequest(req)
+	}
+
+	return exec, close
+}
