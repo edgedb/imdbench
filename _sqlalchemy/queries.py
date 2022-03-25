f6a0cc2a95d7c3901cd761128674634a858ebafb
Mike Bayer <mike_mp@zzzcomputing.com>
2022-03-25 13:53:55 -0400

    update models and queries
    in this commit we use modern APIs and features for the
    ORM load/commit demonstration:
    
    1. use select() for selecting; the "baked queries" extension
       is now legacy as SQLAlchemy's query caching mechanism has
       been generalized to all queries.  A "baked" still has a slight
       per-query edge when the "lambda" feature is used,
       which we could have used here as well without "baked",
       but I would prefer this code illustrate clean and idiomatic
       API use rather than more esoteric speed improvements.
    
    2. bulk updates/deletes use update(), delete()
    
    3. remove use of subqueryload() which is superseded by
       selectinload(), which generates much simpler queries that
       should generally plan better
    
    4. use a with_parent() query instead of lazy="dynamic", this
       relationship style isn't going away but is a little up in
       the air right now
    
    5. set innerjoin=True on relationships that are M2O against
       non-nullable foreign key col
    
    The performance here is not necessarily expected to improve
    or worsen, factors which should improve performance include:
    
    1. setting expire_on_commit=False
    2. using selectinload()
    
    Factors which might degrade performance include:
    
    1. a baked with lambdas is slightly faster than a regular
       statement that isn't built up with lambdas, as the generation
       of the construct as well as the cache key is much faster
       with lambdas.   We hope to increase the use of cython
       in the 2.0 series to optimize cache key generation which hopefully
       will help alot.
    
    Performance is still not optimal for what we can do on the
    persistence side, the ORM now knows how to INSERT..RETURNING
    multiple records in one statement, which is a *huge* performance
    increase over statement-at-a-time, but in 1.4 this is only
    implemented for psycopg2 so far and at the moment the revised
    demo is using asyncpg.  As the multi-insert-returning
    demo here seems to involve only a few records per commit,
    stick with the old way.

--- _sqlalchemy/queries.py
+++ _sqlalchemy/queries.py
@@ -10,14 +10,12 @@ import json
 import random
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
-from sqlalchemy.ext import baked
 import _sqlalchemy.models as m
 
 
 engine = None
 session_factory = None
-bakery = baked.bakery()
-INSERT_PREFIX = 'insert_test__'
+INSERT_PREFIX = "insert_test__"
 
 
 def connect(ctx):
@@ -26,8 +24,9 @@ def connect(ctx):
 
     if session_factory is None:
         engine = sa.create_engine(
-            f'postgresql+asyncpg://sqlalch_bench:edgedbbenchmark@'
-            f'{ctx.db_host}:{ctx.pg_port}/sqlalch_bench?async_fallback=true')
+            f"postgresql+asyncpg://sqlalch_bench:edgedbbenchmark@"
+            f"{ctx.db_host}:{ctx.pg_port}/sqlalch_bench?async_fallback=true"
+        )
         session_factory = orm.sessionmaker(bind=engine, expire_on_commit=False)
 
     return session_factory()
@@ -38,22 +37,16 @@ def close(ctx, sess):
 
 
 def load_ids(ctx, sess):
-    users = (
-        sess.query(m.User)
-        .order_by(sa.func.random())
-        .limit(ctx.number_of_ids)
+    users = sess.scalars(
+        sa.select(m.User).order_by(sa.func.random()).limit(ctx.number_of_ids)
     ).all()
 
-    movies = (
-        sess.query(m.Movie)
-        .order_by(sa.func.random())
-        .limit(ctx.number_of_ids)
+    movies = sess.scalars(
+        sa.select(m.Movie).order_by(sa.func.random()).limit(ctx.number_of_ids)
     ).all()
 
-    people = (
-        sess.query(m.Person)
-        .order_by(sa.func.random())
-        .limit(ctx.number_of_ids)
+    people = sess.scalars(
+        sa.select(m.Person).order_by(sa.func.random()).limit(ctx.number_of_ids)
     ).all()
 
     return dict(
@@ -65,40 +58,50 @@ def load_ids(ctx, sess):
         # generate as many insert stubs as "concurrency" to
         # accommodate concurrent inserts
         insert_user=[INSERT_PREFIX] * ctx.concurrency,
-        insert_movie=[{
-            'prefix': INSERT_PREFIX,
-            'people': [p.id for p in people[:4]],
-        }] * ctx.concurrency,
+        insert_movie=[
+            {
+                "prefix": INSERT_PREFIX,
+                "people": [p.id for p in people[:4]],
+            }
+        ]
+        * ctx.concurrency,
         insert_movie_plus=[INSERT_PREFIX] * ctx.concurrency,
     )
 
 
 def get_user(sess, id):
-    user_query = bakery(lambda sess: sess.query(m.User))
-    user_query += lambda q: q.filter_by(id=sa.bindparam('id'))
-    user = user_query(sess).params(id=id).first()
+    user_query = sa.select(m.User).filter_by(id=id)
+    user = sess.scalars(user_query).first()
 
-    latest_reviews = user.latest_reviews.options(
-        orm.joinedload(m.Review.movie)).limit(10).all()
+    stmt = (
+        sa.select(m.Review)
+        .where(orm.with_parent(user, m.User.latest_reviews))
+        .options(orm.joinedload(m.Review.movie))
+        .limit(10)
+    )
+    latest_reviews = sess.scalars(stmt).all()
 
-    result = json.dumps({
-        'id': user.id,
-        'name': user.name,
-        'image': user.image,
-        'latest_reviews': [
-            {
-                'id': r.id,
-                'body': r.body,
-                'rating': r.rating,
-                'movie': {
-                    'id': r.movie.id,
-                    'image': r.movie.image,
-                    'title': r.movie.title,
-                    'avg_rating': float(r.movie.avg_rating),
+    result = json.dumps(
+        {
+            "id": user.id,
+            "name": user.name,
+            "image": user.image,
+            "latest_reviews": [
+                {
+                    "id": r.id,
+                    "body": r.body,
+                    "rating": r.rating,
+                    "movie": {
+                        "id": r.movie.id,
+                        "image": r.movie.image,
+                        "title": r.movie.title,
+                        "avg_rating": float(r.movie.avg_rating),
+                    },
                 }
-            } for r in latest_reviews
-        ]
-    })
+                for r in latest_reviews
+            ],
+        }
+    )
     return result
 
 
@@ -113,105 +116,99 @@ def get_movie(sess, id):
         else:
             return (rel.list_order, rel.person_rel.last_name)
 
-    baked_query = bakery(lambda sess: (
-        sess.query(m.Movie)
-            .options(
-                orm.subqueryload(m.Movie.directors_rel)
-                .joinedload(m.Directors.person_rel, innerjoin=True),
-
-                orm.subqueryload(m.Movie.cast_rel)
-                .joinedload(m.Cast.person_rel, innerjoin=True),
-
-                orm.subqueryload(m.Movie.reviews)
-                .joinedload(m.Review.author, innerjoin=True),
-            )
+    stmt = (
+        sa.select(m.Movie)
+        .options(
+            orm.selectinload(m.Movie.directors_rel).joinedload(m.Directors.person_rel),
+            orm.selectinload(m.Movie.cast_rel).joinedload(m.Cast.person_rel),
+            orm.selectinload(m.Movie.reviews).joinedload(m.Review.author),
         )
+        .filter_by(id=id)
     )
 
-    baked_query += lambda q: q.filter_by(id=sa.bindparam('id'))
+    movie = sess.scalars(stmt).first()
 
-    movie = baked_query(sess).params(id=id).first()
+    directors = [rel.person_rel for rel in sorted(movie.directors_rel, key=sort_key)]
 
-    directors = [rel.person_rel for rel in
-                 sorted(movie.directors_rel, key=sort_key)]
-
-    cast = [rel.person_rel for rel in
-            sorted(movie.cast_rel, key=sort_key)]
+    cast = [rel.person_rel for rel in sorted(movie.cast_rel, key=sort_key)]
 
     result = {
-        'id': movie.id,
-        'image': movie.image,
-        'title': movie.title,
-        'year': movie.year,
-        'description': movie.description,
-        'avg_rating': float(movie.avg_rating),
-        'directors': [
+        "id": movie.id,
+        "image": movie.image,
+        "title": movie.title,
+        "year": movie.year,
+        "description": movie.description,
+        "avg_rating": float(movie.avg_rating),
+        "directors": [
             {
-                'id': d.id,
-                'full_name': d.full_name,
-                'image': d.image,
-            } for d in directors
+                "id": d.id,
+                "full_name": d.full_name,
+                "image": d.image,
+            }
+            for d in directors
         ],
-        'cast': [
+        "cast": [
             {
-                'id': c.id,
-                'full_name': c.full_name,
-                'image': c.image,
-            } for c in cast
+                "id": c.id,
+                "full_name": c.full_name,
+                "image": c.image,
+            }
+            for c in cast
         ],
-        'reviews': [
+        "reviews": [
             {
-                'id': r.id,
-                'body': r.body,
-                'rating': float(r.rating),
-                'author': {
-                    'id': r.author.id,
-                    'name': r.author.name,
-                    'image': r.author.image,
-                }
-            } for r in sorted(movie.reviews,
-                              key=lambda x: x.creation_time,
-                              reverse=True)
-        ]
+                "id": r.id,
+                "body": r.body,
+                "rating": float(r.rating),
+                "author": {
+                    "id": r.author.id,
+                    "name": r.author.name,
+                    "image": r.author.image,
+                },
+            }
+            for r in sorted(movie.reviews, key=lambda x: x.creation_time, reverse=True)
+        ],
     }
 
     return json.dumps(result)
 
 
 def get_person(sess, id):
-    baked_query = bakery(lambda sess: sess.query(m.Person).options(
-        orm.joinedload(m.Person.acted_in),
-        orm.joinedload(m.Person.directed),
-    ))
+    stmt = (
+        sa.select(m.Person)
+        .options(
+            orm.selectinload(m.Person.acted_in),
+            orm.selectinload(m.Person.directed),
+        )
+        .filter_by(id=id)
+    )
 
-    baked_query += lambda q: q.filter_by(id=sa.bindparam('id'))
-
-    person = baked_query(sess).params(id=id).first()
+    person = sess.scalars(stmt).first()
 
     result = {
-        'id': person.id,
-        'image': person.image,
-        'full_name': person.full_name,
-        'bio': person.bio,
-        'acted_in': [
+        "id": person.id,
+        "image": person.image,
+        "full_name": person.full_name,
+        "bio": person.bio,
+        "acted_in": [
             {
-                'id': m.id,
-                'image': m.image,
-                'title': m.title,
-                'year': m.year,
-                'avg_rating': float(m.avg_rating),
-            } for m in sorted(person.acted_in,
-                              key=lambda x: (x.year, x.title))
+                "id": m.id,
+                "image": m.image,
+                "title": m.title,
+                "year": m.year,
+                "avg_rating": float(m.avg_rating),
+            }
+            for m in sorted(person.acted_in, key=lambda x: (x.year, x.title))
         ],
-        'directed': [
+        "directed": [
             {
-                'id': m.id,
-                'image': m.image,
-                'title': m.title,
-                'year': m.year,
-                'avg_rating': float(m.avg_rating),
-            } for m in sorted(person.directed,
-                              key=lambda x: (x.year, x.title))
+                "id": m.id,
+                "image": m.image,
+                "title": m.title,
+                "year": m.year,
+                "avg_rating": float(m.avg_rating),
+            }
+            for m in sorted(person.directed, key=lambda x: (x.year, x.title))
         ],
     }
 
@@ -219,42 +216,42 @@ def get_person(sess, id):
 
 
 def update_movie(sess, id):
-    stmt = sa.update(
-        m.Movie
-    ).filter_by(
-        id=sa.bindparam('m_id')
-    ).values(
-        title=m.Movie.title + sa.bindparam('suffix')
-    ).returning(
-        m.Movie.id,
-        m.Movie.title,
+    stmt = (
+        sa.update(m.Movie)
+        .filter_by(id=sa.bindparam("m_id"))
+        .values(title=m.Movie.title + sa.bindparam("suffix"))
+        .returning(
+            m.Movie.id,
+            m.Movie.title,
+        )
     )
 
-    result = sess.execute(
-        stmt,
-        dict(m_id=id, suffix=f'---{str(id)[:8]}')
-    ).first()
+    result = sess.execute(stmt, dict(m_id=id, suffix=f"---{str(id)[:8]}")).first()
     # Without this commit, the changes end up being committed outside
     # of where they are timed.
     sess.commit()
 
-    return json.dumps({
-        'id': result[0],
-        'title': result[1],
-    })
+    return json.dumps(
+        {
+            "id": result[0],
+            "title": result[1],
+        }
+    )
 
 
 def insert_user(sess, val):
     num = random.randrange(1_000_000)
-    user = m.User(name=f'{val}{num}', image=f'image_{val}{num}')
+    user = m.User(name=f"{val}{num}", image=f"image_{val}{num}")
     sess.add(user)
     sess.commit()
 
-    return json.dumps({
-        'id': user.id,
-        'name': user.name,
-        'image': user.image,
-    })
+    return json.dumps(
+        {
+            "id": user.id,
+            "name": user.name,
+            "image": user.image,
+        }
+    )
 
 
 def insert_movie(sess, val):
@@ -273,30 +270,31 @@ def insert_movie(sess, val):
     c0 = m.Cast(person_id=val["people"][1], movie_id=movie.id)
     c1 = m.Cast(person_id=val["people"][2], movie_id=movie.id)
     c2 = m.Cast(person_id=val["people"][3], movie_id=movie.id)
-    sess.add(c0)
-    sess.add(c1)
-    sess.add(c2)
+
+    sess.add_all([c0, c1, c2])
+
     sess.commit()
 
     result = {
-        'id': movie.id,
-        'image': movie.image,
-        'title': movie.title,
-        'year': movie.year,
-        'description': movie.description,
-        'directors': [
+        "id": movie.id,
+        "image": movie.image,
+        "title": movie.title,
+        "year": movie.year,
+        "description": movie.description,
+        "directors": [
             {
-                'id': directors.person_rel.id,
-                'full_name': directors.person_rel.full_name,
-                'image': directors.person_rel.image,
+                "id": directors.person_rel.id,
+                "full_name": directors.person_rel.full_name,
+                "image": directors.person_rel.image,
             }
         ],
-        'cast': [
+        "cast": [
             {
-                'id': c.person_rel.id,
-                'full_name': c.person_rel.full_name,
-                'image': c.person_rel.image,
-            } for c in [c0, c1, c2]
+                "id": c.person_rel.id,
+                "full_name": c.person_rel.full_name,
+                "image": c.person_rel.image,
+            }
+            for c in [c0, c1, c2]
         ],
     }
     return json.dumps(result)
@@ -305,114 +303,119 @@ def insert_movie(sess, val):
 def insert_movie_plus(sess, val):
     num = random.randrange(1_000_000)
     director = m.Person(
-        first_name=f'{val}Alice',
-        last_name=f'{val}Director',
-        image=f'{val}image{num}.jpeg',
-        bio='',
+        first_name=f"{val}Alice",
+        last_name=f"{val}Director",
+        image=f"{val}image{num}.jpeg",
+        bio="",
     )
     c0 = m.Person(
-        first_name=f'{val}Billie',
-        last_name=f'{val}Actor',
-        image=f'{val}image{num+1}.jpeg',
-        bio='',
+        first_name=f"{val}Billie",
+        last_name=f"{val}Actor",
+        image=f"{val}image{num+1}.jpeg",
+        bio="",
     )
     c1 = m.Person(
-        first_name=f'{val}Cameron',
-        last_name=f'{val}Actor',
-        image=f'{val}image{num+2}.jpeg',
-        bio='',
+        first_name=f"{val}Cameron",
+        last_name=f"{val}Actor",
+        image=f"{val}image{num+2}.jpeg",
+        bio="",
     )
-    sess.add(director)
-    sess.add(c0)
-    sess.add(c1)
     movie = m.Movie(
-        title=f'{val}{num}',
-        image=f'{val}image{num}.jpeg',
-        description=f'{val}description{num}',
+        title=f"{val}{num}",
+        image=f"{val}image{num}.jpeg",
+        description=f"{val}description{num}",
         year=num,
     )
-    sess.add(movie)
+
+    sess.add_all([
+        director,
+        c0,
+        c1,
+        movie
+    ])
     sess.commit()
 
-    sess.add(m.Directors(person_id=director.id, movie_id=movie.id))
-    sess.add(m.Cast(person_id=c0.id, movie_id=movie.id))
-    sess.add(m.Cast(person_id=c1.id, movie_id=movie.id))
+    sess.add_all([
+        m.Directors(person_id=director.id, movie_id=movie.id),
+        m.Cast(person_id=c0.id, movie_id=movie.id),
+        m.Cast(person_id=c1.id, movie_id=movie.id)
+    ])
+
     sess.commit()
 
     result = {
-        'id': movie.id,
-        'image': movie.image,
-        'title': movie.title,
-        'year': movie.year,
-        'description': movie.description,
-        'directors': [
+        "id": movie.id,
+        "image": movie.image,
+        "title": movie.title,
+        "year": movie.year,
+        "description": movie.description,
+        "directors": [
             {
-                'id': director.id,
-                'full_name': director.full_name,
-                'image': director.image,
+                "id": director.id,
+                "full_name": director.full_name,
+                "image": director.image,
             }
         ],
-        'cast': [
+        "cast": [
             {
-                'id': c.id,
-                'full_name': c.full_name,
-                'image': c.image,
-            } for c in [c0, c1]
+                "id": c.id,
+                "full_name": c.full_name,
+                "image": c.image,
+            }
+            for c in [c0, c1]
         ],
     }
     return json.dumps(result)
 
 
 def setup(ctx, sess, queryname):
-    if queryname == 'update_movie':
-        sess.query(
-            m.Movie
-        ).update(
-            dict(title=sa.func.split_part(m.Movie.title, '---', 1))
+    if queryname == "update_movie":
+        sess.execute(
+            sa.update(m.Movie)
+            .values(title=sa.func.split_part(m.Movie.title, "---", 1))
+            .execution_options(synchronize_session=False)
         )
         sess.commit()
-    elif queryname == 'insert_user':
-        sess.query(
-            m.User
-        ).filter(
-            m.User.name.like(f'{INSERT_PREFIX}%')
-        ).delete(synchronize_session=False)
+    elif queryname == "insert_user":
+        sess.execute(
+            sa.delete(m.User)
+            .where(m.User.name.like(f"{INSERT_PREFIX}%"))
+            .execution_options(synchronize_session=False)
+        )
         sess.commit()
-    elif queryname in {'insert_movie', 'insert_movie_plus'}:
-        # XXX: I just gave up on trying to figure out how to bulk delete
-        # all this in SQLAlchemy proper. Trying to filter the
-        # relationships based on the Movie they target didn't seem to
-        # work. So I'll use a raw query.
-        sess.execute('''
-            DELETE FROM
-                "directors" as D
-            USING
-                "movie" as M
-            WHERE
-                D.movie_id = M.id AND M.image LIKE 'insert_test__%';
+    elif queryname in {"insert_movie", "insert_movie_plus"}:
 
-            DELETE FROM
-                "cast" as C
-            USING
-                "movie" as M
-            WHERE
-                C.movie_id = M.id AND M.image LIKE 'insert_test__%';
-
-            DELETE FROM
-                "movie" as M
-            WHERE
-                M.image LIKE 'insert_test__%';
-
-            DELETE FROM
-                "person" as P
-            WHERE
-                P.image LIKE 'insert_test__%';
-        ''')
+        sess.execute(
+            sa.delete(m.Directors)
+            .where(m.Directors.movie_rel)
+            .where(m.Movie.image.like(f"{INSERT_PREFIX}%"))
+            .execution_options(synchronize_session=False)
+        )
+        sess.execute(
+            sa.delete(m.Cast)
+            .where(m.Cast.movie_rel)
+            .where(m.Movie.image.like(f"{INSERT_PREFIX}%"))
+            .execution_options(synchronize_session=False)
+        )
+        sess.execute(
+            sa.delete(m.Movie)
+            .where(m.Movie.image.like(f"{INSERT_PREFIX}%"))
+            .execution_options(synchronize_session=False)
+        )
+        sess.execute(
+            sa.delete(m.Person)
+            .where(m.Person.image.like(f"{INSERT_PREFIX}%"))
+            .execution_options(synchronize_session=False)
+        )
         sess.commit()
 
 
 def cleanup(ctx, sess, queryname):
-    if queryname in {'update_movie', 'insert_user', 'insert_movie',
-                     'insert_movie_plus'}:
+    if queryname in {
+        "update_movie",
+        "insert_user",
+        "insert_movie",
+        "insert_movie_plus",
+    }:
         # The clean up is the same as setup for mutation benchmarks
         setup(ctx, sess, queryname)
