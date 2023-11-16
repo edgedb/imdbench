ba3793c4c6a2ccb858b7b609bacb76d0d378c7c6
Fantix King <fantix.king@gmail.com>
2023-11-16 13:11:22 -0500

    Add load planetscale for Prisma

---
+++ _postgres/loaddata_planetscale.py
@@ -0,0 +1,223 @@
+#
+# Copyright (c) 2019 MagicStack Inc.
+# All rights reserved.
+#
+# See LICENSE for details.
+##
+
+
+import argparse
+import asyncio
+import ssl
+
+import aiomysql
+import collections
+import datetime
+import json
+import os
+import progress.bar
+
+
+class Pool:
+
+    _STOP = object()
+
+    def __init__(self, data, *, concurrency: int):
+        self._concurrency = concurrency
+
+        self._results = asyncio.Queue()
+
+        self._queue = asyncio.Queue()
+        for piece in data:
+            self._queue.put_nowait(piece)
+        for _ in range(self._concurrency):
+            self._queue.put_nowait(self._STOP)
+
+        self._workers = []
+
+    def _start(self):
+        for _ in range(self._concurrency):
+            self._workers.append(
+                asyncio.create_task(self._worker()))
+
+    async def _worker(self):
+        async with await aiomysql.connect(
+            user=os.environ["PLANETSCALE_USER"],
+            db=os.environ["PLANETSCALE_DATABASE"],
+            password=os.environ["PLANETSCALE_PASSWORD"],
+            host=os.environ["PLANETSCALE_HOST"],
+            port=3306,
+            ssl=ssl.create_default_context(),
+        ) as con:
+            while True:
+                args = await self._queue.get()
+                if args is self._STOP:
+                    self._results.put_nowait(self._STOP)
+                    break
+                cb, *args = args
+
+                try:
+                    async with con.cursor() as cur:
+                        await cur.execute(args[0], args[1:])
+                        i = cur.lastrowid
+                except Exception as e:
+                    self._results.put_nowait(e)
+                else:
+                    if cb is not None:
+                        cb(i)
+                    self._results.put_nowait(True)
+
+    @classmethod
+    async def map(cls, data, *, concurrency: int, label: str):
+        pool = cls(data, concurrency=concurrency)
+        pool._start()
+
+        bar = progress.bar.Bar(label[:15].ljust(15), max=len(data))
+
+        stop_cnt = 0
+        while True:
+            piece = await pool._results.get()
+            if piece is cls._STOP:
+                stop_cnt += 1
+                if stop_cnt == concurrency:
+                    bar.finish()
+                    return
+            elif isinstance(piece, Exception):
+                raise piece
+            else:
+                bar.next()
+
+
+async def import_data(data: dict):
+    concurrency = 32
+
+    users = data['user']
+    reviews = data['review']
+    movies = data['movie']
+    people = data['person']
+
+    users_data = [
+        (
+            lambda realid, origid=u['id']: map_ids(realid, origid, 'user'),
+            '''
+            INSERT INTO users(name, image) VALUES (%s, %s);
+            ''',
+            u['name'],
+            u['image'],
+        ) for u in users
+    ]
+
+    movies_data = [
+        (
+            lambda realid, origid=m['id']: map_ids(realid, origid, 'movie'),
+            '''
+            INSERT INTO movies(image, title, year, description)
+            VALUES (%s, %s, %s, %s);
+            ''',
+            m['image'],
+            m['title'],
+            m['year'],
+            m['description'],
+        ) for m in movies
+    ]
+
+    people_data = [
+        (
+            lambda realid, origid=p['id']: map_ids(realid, origid, 'person'),
+            '''
+            INSERT INTO persons(first_name, middle_name, last_name, image, bio)
+            VALUES (%s, %s, %s, %s, %s);
+            ''',
+            p['first_name'],
+            p['middle_name'],
+            p['last_name'],
+            p['image'],
+            p['bio'],
+        ) for p in people
+    ]
+
+    await Pool.map(users_data, concurrency=concurrency, label='users')
+    await Pool.map(movies_data, concurrency=concurrency, label='movies')
+    await Pool.map(people_data, concurrency=concurrency, label='people')
+
+    directors_data = [
+        (
+            None,
+            '''
+            INSERT INTO directors(list_order, person_id, movie_id)
+            VALUES (%s, %s, %s);
+            ''',
+            d['list_order'],
+            ids_map['person'][d['person_id']],
+            ids_map['movie'][d['movie_id']],
+        ) for d in data['directors']
+    ]
+
+    actors_data = [
+        (
+            None,
+            '''
+            INSERT INTO actors(list_order, person_id, movie_id)
+            VALUES (%s, %s, %s);
+            ''',
+            c.get('list_order', None),
+            ids_map['person'][c['person_id']],
+            ids_map['movie'][c['movie_id']],
+        ) for c in data['cast']
+    ]
+
+    await Pool.map(directors_data, concurrency=concurrency, label='directors')
+    await Pool.map(actors_data, concurrency=concurrency, label='actors')
+
+    reviews_data = [
+        (
+            None,
+            '''
+            INSERT INTO
+                reviews(body, rating, creation_time, author_id, movie_id)
+            VALUES (%s, %s, %s, %s, %s);
+            ''',
+            r['body'],
+            r['rating'],
+            r['creation_time'],
+            ids_map['user'][r['author_id']],
+            ids_map['movie'][r['movie_id']],
+        ) for r in reviews
+    ]
+
+    await Pool.map(reviews_data, concurrency=concurrency, label='reviews')
+
+
+ids_map = {'person': {}, 'user': {}, 'movie': {}}
+
+
+def map_ids(realid, origid, cat):
+    assert origid not in ids_map[cat]
+    ids_map[cat][origid] = realid
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Load a specific fixture, old data will be purged.')
+    parser.add_argument('filename', type=str,
+                        help='The JSON dataset file')
+
+    args = parser.parse_args()
+
+    with open(args.filename, 'rt') as f:
+        records = json.load(f)
+
+    data = collections.defaultdict(list)
+    for rec in records:
+        rtype = rec['model'].split('.')[-1]
+        datum = rec['fields']
+        if 'pk' in rec:
+            datum['id'] = rec['pk']
+        # convert datetime
+        if rtype == 'review':
+            datum['creation_time'] = datetime.datetime.fromisoformat(
+                datum['creation_time'])
+
+        data[rtype].append(datum)
+
+    asyncio.run(import_data(data))
